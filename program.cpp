aaad1f30279978e8072c901eb0706cf2aec1aa91
felipemagalhaes <ffelipemagallhaes@gmail.com>
2025-06-30 21:11:10 -0400

    Alpha 0.1

---
+++ program.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+  std::cout << "10 Best Shounen protagonists: \n";
+  std::cout << " - Saitama\n - Ichigo \n - Guts \n - Kaneki \n - Eren \n - Alucard \n - Gon \n - Yusuke \n - Yu \n - Shinji \n";
+  return 0;
+}
